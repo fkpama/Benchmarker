ef7579cc08b44950ca065aef65f352f64f26a07b
Frederic Kpama <frederic.kpama@primadiag.com>
2023-05-23 20:47:16 +0200

    Ok

---
+++ build/scripts/AutoVer.ps1
@@ -0,0 +1,33 @@
+﻿param(
+    [Parameter(Position=1)][string]$TagPrefix = 'pre-prelease/v',
+    [Parameter()][string]$PreRelease = 'preview'
+)
+
+$ErrorActionPreference='Stop'
+
+[string]$current = &dotnet minver -t $TagPrefix -i -p $PreRelease --auto-increment patch
+$next = &dotnet minver -t $TagPrefix -p $PreRelease --auto-increment patch
+
+$idx=$current.LastIndexOf('.')
+$height = $current.Substring($idx + 1)
+if ( -not $height -match '^[\d\.]+$' ) {
+    $height = 0
+    $next = "$next.0"
+}
+Write-Output "Computed: $current => $next"
+if ($current -ne $next)
+{
+    $idx=$current.LastIndexOf('.')
+    $height = $current.Substring($idx + 1)
+    if ( $height -match '^[\d\.]+$' ) {
+        [string]$base=$current.Substring(0, $idx)
+        $next="$base.$(1 + [int]$height)"
+    }
+    
+    Write-Output "NEXT: $current => $next"
+    Write-Output "##vso[task.setvariable variable=PackageVersion;]$next"
+}
+else
+{
+    Write-Output "Nothing to do"
+}
